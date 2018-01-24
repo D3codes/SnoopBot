71a4a6f319a2ab8b83f9e654ba24079c809fffd9
David Freeman <d526f861@gmail.com>
2018-01-23 22:31:26 -0600

    stated building snoopbot controller

--- snoopbot.py
+++ snoopbot.py
@@ -1,1 +1,21 @@
 import praw
+import sqlite3
+import time
+import snoopsnoo
+
+USERNAME = 'snoop-bot'
+
+print('Opening snoop-bot database')
+sql = sqlite3.connect('snoop.db')
+cur = sql.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
+sql.commit()
+
+def handle_ratelimit(func, *args, **kwargs):
+    while True:
+        try:
+            func(*args, **kwargs)
+            break
+        except praw.errors.RateLimitExceeded as error:
+            print('\tSleeping for %d seconds' % error.sleep_time)
+            time.sleep(error.sleep_time)
