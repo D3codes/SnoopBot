7c89ad60ffc718ab9954c39e6e22743be6064049
David Freeman <d526f861@gmail.com>
2018-01-23 21:51:48 -0600

    finished synopsis parsing

--- snoopsnoo.py
+++ snoopsnoo.py
@@ -5,8 +5,8 @@ baseURL = 'https://snoopsnoo.com/u/'
 
 summary = ''
 synopsis = ''
-userInfo = {}
-userGuesses = {}
+userSummary = {}
+userSynopsis = {}
 
 def search(user):
     browser = webdriver.PhantomJS()
@@ -20,44 +20,56 @@ def search(user):
     parseSynopsis()
 
 def parseSummary():
-    global userInfo
+    global userSummary
     #Redditor since
-    userInfo['signUpDate'] = summary.find('span', {"id": "data-signup_date"}).string
+    userSummary['signUpDate'] = summary.find('span', {"id": "data-signup_date"}).string
     #Longest period between two consecutive posts
-    userInfo['lurkPeriodHumanized'] = summary.find('span', {"id": "data-lurk_period_humanized"}).string
-    userInfo['lurkPeriodDates'] = summary.find('span', {"id": "data-lurk_period_dates"}).string
+    userSummary['lurkPeriodHumanized'] = summary.find('span', {"id": "data-lurk_period_humanized"}).string
+    userSummary['lurkPeriodDates'] = summary.find('span', {"id": "data-lurk_period_dates"}).string
     #Gilded
-    userInfo['gildedPosts'] = summary.find('span', {"id": "data-submissions_gilded"}).string
-    userInfo['gildedComments'] = summary.find('span', {"id": "data-comments_gilded"}).string
+    userSummary['gildedPosts'] = summary.find('span', {"id": "data-submissions_gilded"}).string
+    userSummary['gildedComments'] = summary.find('span', {"id": "data-comments_gilded"}).string
     #Submission karma
-    userInfo['postKarma'] = summary.find('span', {"id": "data-submission_karma"}).string
-    userInfo['totalPosts'] = summary.find('span', {"id": "data-total_submissions"}).string
-    userInfo['karmaPerPost'] = summary.find('span', {"id": "data-average_submission_karma"}).string
-    userInfo['comparePostKarma'] = summary.find('span', {"id": "data-compare_submission_karma"}).string
+    userSummary['postKarma'] = summary.find('span', {"id": "data-submission_karma"}).string
+    userSummary['totalPosts'] = summary.find('span', {"id": "data-total_submissions"}).string
+    userSummary['karmaPerPost'] = summary.find('span', {"id": "data-average_submission_karma"}).string
+    userSummary['comparePostKarma'] = summary.find('span', {"id": "data-compare_submission_karma"}).string
     #Comment karma
-    userInfo['commentKarma'] = summary.find('span', {"id": "data-comment_karma"}).string
-    userInfo['totalComments'] = summary.find('span', {"id": "data-total_comments"}).string
-    userInfo['karmaPerComment'] = summary.find('span', {"id": "data-average_comment_karma"}).string
-    userInfo['compareCommentKarma'] = summary.find('span', {"id": "data-compare_comment_karma"}).string
+    userSummary['commentKarma'] = summary.find('span', {"id": "data-comment_karma"}).string
+    userSummary['totalComments'] = summary.find('span', {"id": "data-total_comments"}).string
+    userSummary['karmaPerComment'] = summary.find('span', {"id": "data-average_comment_karma"}).string
+    userSummary['compareCommentKarma'] = summary.find('span', {"id": "data-compare_comment_karma"}).string
     #Best comment
     bestComment = summary.find('span', {"id": "data-best_comment"}).parent.parent
-    userInfo['bestCommentText'] = bestComment.findAll('p')[1].string
-    userInfo['bestCommentLink'] = bestComment.find('a').get('href')
+    userSummary['bestCommentText'] = bestComment.findAll('p')[1].string
+    userSummary['bestCommentLink'] = bestComment.find('a').get('href')
     #Worst comment
     worstComment = summary.find('span', {"id": "data-worst_comment"}).parent.parent
-    userInfo['worstCommentText'] = worstComment.findAll('p')[1].string
-    userInfo['worstCommentLink'] = worstComment.find('a').get('href')
+    userSummary['worstCommentText'] = worstComment.findAll('p')[1].string
+    userSummary['worstCommentLink'] = worstComment.find('a').get('href')
     #Best post
     bestPost = summary.find('span', {"id": "data-best_submission"})
-    userInfo['bestPostText'] = bestPost.contents[0]
-    userInfo['bestPostLink'] = bestPost.find('a').get('href')
+    userSummary['bestPostText'] = bestPost.contents[0]
+    userSummary['bestPostLink'] = bestPost.find('a').get('href')
     #Worst post
     worstPost = summary.find('span', {"id": "data-worst_submission"})
-    userInfo['worstPostText'] = worstPost.contents[0]
-    userInfo['worstPostLink'] = worstPost.find('a').get('href')
+    userSummary['worstPostText'] = worstPost.contents[0]
+    userSummary['worstPostLink'] = worstPost.find('a').get('href')
 
 def parseSynopsis():
-    global userGuesses
+    global userSynopsis
     rows = synopsis.findAll('div', {"class": "row"})
     for row in rows:
-        print(row.contents[0].string)
+        key = row.contents[0].string
+        contents = row.contents[1].findAll('span', {"class": "content"})
+        values = []
+        if key in userSynopsis:
+            values = userSynopsis[key]
+        for content in contents:
+            values.append(content.string)
+        userSynopsis[key] = values
+
+def getSynopsis():
+    for key in userSynopsis:
+        print(key)
+        print(userSynopsis[key])
